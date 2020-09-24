c5c3bffc62a51af2c50b1eeda47606dec07e3a4e
kennygloggins <kenneth.t.smith@gmail.com>
2020-09-24 16:19:05 -0400

    first commit

---
+++ dash_plotly.py
@@ -0,0 +1,104 @@
+import pandas as pd
+from pymongo import MongoClient
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from config import mongo
+
+labels = {
+    "water_level": ["height", "Feet"],
+    "water_temperature": ["temp", "Temperature(defrees F)"],
+    "wind": ["Speed", "Gust", "Direction"],
+}
+
+app = dash.Dash(__name__)
+
+server = MongoClient(mongo)
+db = server.tidewater_db
+
+# pd.set_option("display.max_columns", 10)
+# pd.set_option("display.max_rows", 85)
+
+# df.head()
+# df.info()
+# print(df[["time", "water_level data"]])
+
+app.layout = html.Div(
+    [
+        html.H1("Kayak?", style={"text-align": "center"}),
+        dcc.Dropdown(
+            id="slct_cat",
+            options=[
+                {"label": "Water Level", "value": "water_level"},
+                {"label": "Water Temp", "value": "water_temperature"},
+                {"label": "Wind", "value": "wind"},
+            ],
+            multi=False,
+            value="water_level",
+            style={"width": "40%"},
+        ),
+        html.Div(id="output_container", children=[]),
+        html.Br(),
+        dcc.Graph(id="tide_op", figure={}),
+    ]
+)
+
+
+@app.callback(
+    [
+        Output(component_id="output_container", component_property="children"),
+        Output(component_id="tide_op", component_property="figure"),
+    ],
+    [Input(component_id="slct_cat", component_property="value")],
+)
+def update_graph(option_slctd):
+    df = pd.DataFrame(list(db[option_slctd].find()))
+    print(option_slctd)
+
+    container = "The category chosen by user was: {}".format(option_slctd)
+
+    dff = df.copy()
+    # dff = dff[dff["Category"] == option_slctd]
+    # dff = dff[dff["Affected by"] == "Varroa_mites"]
+
+    # Plotly Express
+    fig = go.Figure()
+
+    if option_slctd == "wind":
+        fig.add_trace(
+            go.Scatter(
+                x=df["time"],
+                y=df[f"{labels[option_slctd][0]}"],
+                name=f"{labels[option_slctd][0]}",
+                line=dict(color="firebrick", width=4),
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=df["time"],
+                y=df[f"{labels[option_slctd][1]}"],
+                name=f"{labels[option_slctd][1]}",
+                line=dict(color="blue", width=4),
+            )
+        )
+        fig.update_layout(xaxis_title="Time", yaxis_title="MPH")
+    else:
+        fig.add_trace(
+            go.Scatter(
+                x=df["time"],
+                y=df[f"{labels[option_slctd][0]}"],
+                name=f"{option_slctd} data",
+                line=dict(color="blue", width=4),
+            )
+        )
+        fig.update_layout(xaxis_title="Time", yaxis_title=labels[option_slctd][1])
+
+    return container, fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ tidewater_request.py
@@ -0,0 +1,130 @@
+import requests
+from pymongo import MongoClient
+from config import mongo
+
+# Setup connection to mongodb collection
+server = MongoClient(mongo)
+db = server.tidewater_db
+
+# collection.insert_one({'x': 1}).inserted_id
+
+products = [
+    "water_level",
+    "water_temperature",
+    "wind",
+    # 'visibility',
+    # 'humidity',
+    # 'currents'
+]
+
+label = [
+    "height",
+    "temp",
+]
+
+
+def req(i):
+    # list of items we want to request from NOAA CO-OP api
+    payload = {
+        # 'begin_date': f'{begin_date}',
+        # 'end_date': f'{end_date}',
+        "date": "recent",
+        "station": "8570283",
+        "product": products[i],
+        "time_zone": "lst",
+        "application": "kgl",
+        "format": "json",
+        "units": "english",
+        "datum": "STND",
+    }
+    r = requests.get(
+        "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter", params=payload
+    ).json()
+    mongoIns(r, i)
+
+
+def mongoIns(r, i):
+    if i < 2:
+        for time in range(len(r["data"])):
+            if db[products[i]].find_one({"time": r["data"][time]["t"]}):
+                db[products[i]].update_one(
+                    {"time": r["data"][time]["t"]},
+                    {"$set": {f"{label[i]}": r["data"][time]["v"]}},
+                )
+            else:
+                lst = {}
+                lst["info"] = r["metadata"]
+                lst["time"] = r["data"][time]["t"]
+                lst[f"{label[i]}"] = r["data"][time]["v"]
+                db[products[i]].insert_one(lst).inserted_id
+
+    else:
+        for time in range(len(r["data"])):
+            if db[products[i]].find_one({"time": r["data"][time]["t"]}):
+                db[products[i]].update_one(
+                    {"time": r["data"][time]["t"]},
+                    {
+                        "$set": {
+                            "Speed": r["data"][time]["s"],
+                            "Direction": r["data"][time]["dr"],
+                            "Gust": r["data"][time]["g"],
+                        }
+                    },
+                )
+            else:
+                lst = {}
+                lst["info"] = r["metadata"]
+                lst["time"] = r["data"][time]["t"]
+                lst["Speed"] = r["data"][time]["s"]
+                lst["Direction"] = r["data"][time]["dr"]
+                lst["Gust"] = r["data"][time]["g"]
+                db[products[i]].insert_one(lst).inserted_id
+
+
+# def mongoIns(r, i):
+#     if i < 2:
+#         for time in range(len(r["data"])):
+#             if db.tide_data.find_one({"time": r["data"][time]["t"]}):
+#                 db.tide_data.update_one(
+#                     {"time": r["data"][time]["t"]},
+#                     {"$set": {f"{products[i]} data": r["data"][time]["v"]}},
+#                 )
+#             else:
+#                 lst = {}
+#                 lst["info"] = r["metadata"]
+#                 lst["time"] = r["data"][time]["t"]
+#                 lst[f"{products[i]} data"] = r["data"][time]["v"]
+#                 db.tide_data.insert_one(lst).inserted_id
+#     else:
+#         for time in range(len(r["data"])):
+#             if db.tide_data.find_one({"time": r["data"][time]["t"]}):
+#                 db.tide_data.update_one(
+#                     {"time": r["data"][time]["t"]},
+#                     {
+#                         "$set": {
+#                             f"{products[i]} data": {
+#                                 "Speed": r["data"][time]["s"],
+#                                 "Direction": r["data"][time]["dr"],
+#                                 "Gust": r["data"][time]["g"],
+#                             }
+#                         }
+#                     },
+#                 )
+#             else:
+#                 lst = {}
+#                 lst["info"] = r["metadata"]
+#                 lst["time"] = r["data"][time]["t"]
+#                 lst[f"{products[i]} data"] = {
+#                     "Speed": r["data"][time]["s"],
+#                     "Direction": r["data"][time]["dr"],
+#                     "Gust": r["data"][time]["g"],
+#                 }
+#                 db.tide_data.insert_one(lst).inserted_id
+
+
+if __name__ == "__main__":
+
+    for i in range(3):
+        req(i)
+
+# db.tide_data.createIndex( { "time": 1 }, {unique:true} )
